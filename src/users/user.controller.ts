fd607b8039f2601491321dc03601eb731cf2245d
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2024-04-16 12:49:47 -0300

    create DTO

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { CreateUserDTO } from "./DTO/create-user";
 @Controller('users')
 export class UserController {
     @Post()
-    async create(@Body() body) {
-        return {};
+    async create(@Body() {name, email, password}: CreateUserDTO) {
+        return {name, email};
     }
     @Get()
     async list(){
@@ -12,5 +13,27 @@ export class UserController {
     @Get(':id')
     async show(@Param() params){
         return {user:{}, params}
+    }
+    @Put(':id')
+    async update(@Body() body, @Param() param){
+        return {
+            method: 'Put',
+            body,
+            param
+        }
+    }
+    @Patch(':id')
+    async updatePartial(@Body() body,@Param() param){
+        return {
+            method: 'Param',
+            body,
+            param
+        }
+    }
+    @Delete(':id')
+    async delete(@Param() param){
+        return {                        
+            param
+        }
     }
 }
